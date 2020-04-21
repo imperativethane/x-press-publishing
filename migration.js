843511f6d0713799bcf0606ca0e6cf6fd93e91df
Derek Millar <derek.millartech@gmail.com>
2020-04-21 11:52:38 +0100

    Add Issue database

--- migration.js
+++ migration.js
@@ -2,10 +2,7 @@ const sqlite3 = require('sqlite3');
 
 const db = new sqlite3.Database('./database.sqlite');
 
-db.serialize(function() {
-    db.run('DROP TABLE IF EXISTS `Artist`');
-    db.run('DROP TABLE IF EXISTS `Series`');
-    
+db.serialize(function() {    
     db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
     '`id` INTEGER NOT NULL, ' +
     '`name` TEXT NOT NULL, ' +
@@ -18,4 +15,14 @@ db.serialize(function() {
     '`id` INTEGER NOT NULL PRIMARY KEY, ' +
     '`name` TEXT NOT NULL, ' +
     '`description` TEXT NOT NULL )');
+
+    db.run('CREATE TABLE IF NOT EXISTS `Issue` ( ' +
+    '`id` INTEGER NOT NULL PRIMARY KEY, ' + 
+    '`name` TEXT NOT NULL, ' + 
+    '`issue_number` INTEGER NOT NULL, ' +
+    '`publication_date` TEXT NOT NULL, ' +
+    '`artist_id` INTEGER NOT NULL, ' +
+    '`series_id` INTEGER NOT NULL, ' +
+    'FOREIGN KEY(artist_id) REFERENCES Arist(id), ' +
+    'FOREIGN KEY(series_id) REFERENCES Series(id) )');
 });
